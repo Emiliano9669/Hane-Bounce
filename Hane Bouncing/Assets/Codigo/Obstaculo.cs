8e552629897bbf80b3183a89a0969bb3878ffc19
Emiliano <emiliano9669@gmail.com>
2020-01-09 15:53:24 -0300

    script de spawn listo

---
+++ Hane Bouncing/Assets/Codigo/Obstaculo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+
+    public int speed;
+    public float limitLifePositionY;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
+        if (this.transform.position.y < limitLifePositionY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Hane Bouncing/Assets/Codigo/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public Transform pos;
+    public Text adminReference;
+    public float timeLapseToSpawn;
+
+    private float timeToSpawn = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToSpawn += Time.deltaTime;
+        adminReference.text = timeToSpawn.ToString("0");
+        if (timeToSpawn > timeLapseToSpawn)
+        {
+            print("spawneamos!");
+            Instantiate(prefab, Vector3Random(), prefab.gameObject.transform.rotation);
+            timeToSpawn = 0;
+        }
+    }
+
+
+    private Vector3 Vector3Random()
+    {
+        float x = Random.Range(-2.2f, 2.2f);       
+        return new Vector3(x, 7, 0);
+    }
+
+}
